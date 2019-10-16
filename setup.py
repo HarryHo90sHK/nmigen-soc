cbe0c2e682b95927363ddd02930a3f0e39a13520
whitequark <whitequark@whitequark.org>
2019-10-16 15:29:13 +0000

    memory: pulled from upstream - branching out

--- setup.py
+++ setup.py
@@ -3,8 +3,15 @@ from setuptools import setup, find_packages
 
 def scm_version():
     def local_scheme(version):
-        return version.format_choice("+{node}", "+{node}.dirty")
-    return {"version_scheme": "guess-next-dev", "local_scheme": local_scheme}
+        if version.tag and not version.distance:
+            return version.format_with("")
+        else:
+            return version.format_choice("+{node}", "+{node}.dirty")
+    return {
+        "relative_to": __file__,
+        "version_scheme": "guess-next-dev",
+        "local_scheme": local_scheme
+    }
 
 
 setup(
@@ -16,7 +23,7 @@ setup(
     #long_description="""TODO""",
     license="BSD",
     setup_requires=["setuptools_scm"],
-    install_requires=["nmigen", "nmigen-stdio"],
+    install_requires=["nmigen~=0.1.rc1"],
     packages=find_packages(),
     project_urls={
         "Source Code": "https://github.com/m-labs/nmigen-soc",
